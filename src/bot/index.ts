84ca6b6d11a976fbb81113ef07d9bd3149185655
RashingPro <rashingpro@yandex.ru>
2025-08-23 00:30:34 +0300

    feat: add constructor to Bot class

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,3 +1,10 @@
+import { isModuleClass } from "@/decorators/module";
+
 export default class Bot {
+    constructor(rootModule: any) {
+        if (!isModuleClass(rootModule.constructor))
+            throw new Error(`Module class must be decorated with @Module() (caused by ${rootModule.name})`);
+    }
+
     async run() {}
 }
