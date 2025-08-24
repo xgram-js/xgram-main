d97731a3e64a69559b0861199891b5d9e27e2d40
RashingPro <rashingpro@yandex.ru>
2025-08-24 20:32:17 +0300

    refactor(utils): add logging of provider's dependencies to `dependencyTreeToString` function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
 import { DependencyTreeNode, ModuleImportTreeNode } from "@/decorators/module";
 import archy from "archy";
 import chalk from "chalk";
+import { Class } from "@/types/class";
 
 export function moduleImportTreeToString(tree: ModuleImportTreeNode): string {
     const fn = (tree: ModuleImportTreeNode): archy.Data => {
@@ -12,18 +13,20 @@ export function moduleImportTreeToString(tree: ModuleImportTreeNode): string {
     return archy(fn(tree));
 }
 
+export function dependenciesToString(provider: Class): archy.Data {
+    const deps = Reflect.getOwnMetadata("design:paramtypes", provider) ?? [];
+    const depsFormatted = deps.map(dependenciesToString);
+    return {
+        label: chalk.yellow(provider.name),
+        nodes: depsFormatted
+    };
+}
+
 export function dependencyTreeToString(tree: DependencyTreeNode): string {
     const fn = (tree: DependencyTreeNode): archy.Data => {
         return {
             label: chalk.cyan(tree.thisModule.name),
-            nodes: [
-                ...tree.providersOwn.map(v => {
-                    return {
-                        label: chalk.yellow(v.name)
-                    };
-                }),
-                ...tree.children.map(fn)
-            ]
+            nodes: [...tree.providersOwn.map(dependenciesToString), ...tree.children.map(fn)]
         };
     };
     return archy(fn(tree));
