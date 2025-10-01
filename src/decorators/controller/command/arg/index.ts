99a988baee7cd1c91dbc61c8011d7af0cfce223c
RashingPro <rashingpro@yandex.ru>
2025-10-01 19:29:08 +0300

    refactor: clean up logs

--- src/decorators/controller/command/arg/index.ts
+++ src/decorators/controller/command/arg/index.ts
@@ -58,9 +58,6 @@ export default function Arg(options?: ArgOptions): ParameterDecorator {
                 `First argument of @Command() method must be a context. It can not be decorated with @Arg() (caused by ${chalk.green(cls.name)})`
             );
 
-        console.log(Reflect.getMetadataKeys(target, propertyKey));
-        console.log(Reflect.getOwnMetadata("design:paramtypes", target, propertyKey));
-
         const paramTypes = Reflect.getOwnMetadata("design:paramtypes", target, propertyKey);
 
         const currentArgs = (Reflect.getOwnMetadata(COMMAND_ARGS, method) as ArgDefinitionMetadata[]) ?? [];
